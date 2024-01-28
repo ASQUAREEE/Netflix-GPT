3410f9ee91747ee2df1f4dd63bc560c726cf7fe3
Ashher Ali <ashher868@gmail.com>
2024-01-28 10:17:15 +0530

    Merge unrelated histories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className='font-bold text-3xl'>
-      hello everyone
-    </div>
+    
+    <Body />
   );
 }
 
